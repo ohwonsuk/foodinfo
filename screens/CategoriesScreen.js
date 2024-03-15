dbb76418a9868ffcf7c674f994b8f010b86f5bbf
ohwonsuk <wsoh51@gmail.com>
2024-03-15 22:23:39 +0900

    food category 불러오기

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,23 @@
+import { FlatList } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+// 컴포넌트 reload시 매번 실행되지 않도록 컴포넌트 밖에 정의
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+}
+
+function CategoriesScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+    />
+  );
+}
+
+export default CategoriesScreen;
