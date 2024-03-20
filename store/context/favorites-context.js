235ebe985f82a6d0058638f5334e74336e2cefc9
ohwonsuk <wsoh51@gmail.com>
2024-03-20 23:36:55 +0900

    Context 정의 및 앱 전체에 적용하기

---
+++ store/context/favorites-context.js
@@ -0,0 +1,35 @@
+import { createContext, useState } from "react";
+
+export const FavoritesContext = createContext({
+  ids: [],
+  addFavorites: (id) => {},
+  removeFavorites: (id) => {},
+});
+
+function FavoritesContextProvider({ children }) {
+  const [favoritesIds, setFavoritesIds] = useState([]);
+
+  function addFavorites(id) {
+    setFavoritesIds((currentFavIds) => [...currentFavIds, id]);
+  }
+
+  function removeFavorites() {
+    setFavoritesIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId != id)
+    );
+  }
+
+  const value = {
+    ids: favoritesIds,
+    addFavorites: addFavorites,
+    removeFavorites: removeFavorites,
+  };
+
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+}
+
+export default FavoritesContextProvider;
